d3276883746e73f7731aba85ae6a72bf36a59e37
Terrell <twrigh4911@gmail.com>
2020-09-04 06:23:27 -0400

    n

--- public/parallax.js
+++ public/parallax.js
@@ -1,9 +1,3 @@
-const parallax = document.getElementById('parallax');
-
-
-window.addEventListener("scroll", function(e){
-
-  let offset = window.pageYOffset;
-  let scroll = offset;
-
+$(document).ready(function() {
+  $('#pagepiling').pagepiling();
 });
